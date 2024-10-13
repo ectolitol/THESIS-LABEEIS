5baef4b992bbf9672c263488fc25a029fa73adbe
ectolitol <tolitolerikaa@gmail.com>
2024-10-13 14:38:29 +0800

    final na

--- frontend/src/config/axiosConfig.jsx
+++ frontend/src/config/axiosConfig.jsx
@@ -2,14 +2,14 @@ import axios from 'axios';
 
 // Set the base URL dynamically based on the hostname
 axios.defaults.baseURL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:4000/' 
+  ? 'http://47.129.7.196:4000/' 
   : ''; 
 // 47.129.7.196
 // Ensure credentials (cookies, auth headers) are sent with each request if necessary
 axios.defaults.withCredentials = true;
 
 export const imageBaseURL = window.location.hostname === 'localhost' 
-  ? 'http://localhost:4000' 
+  ? 'http://47.129.7.196:4000' 
   : '';
 
 
