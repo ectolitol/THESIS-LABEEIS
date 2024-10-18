803e8f1742c091377664501359d536835f37661e
ectolitol <tolitolerikaa@gmail.com>
2024-10-18 11:44:18 +0800

    "

--- frontend/src/config/axiosConfig.jsx
+++ frontend/src/config/axiosConfig.jsx
@@ -3,14 +3,14 @@ import axios from "axios";
 // Set the base URL dynamically based on the environment
 axios.defaults.baseURL =
   window.location.hostname === "localhost"
-    ? "https://52.221.193.216:4000/" // Development (local)
-    : "https://52.221.193.216:4000/"; // Production (use the same base URL)
+    ? "https://api.pupeelms.com:4000/" // Development (local)
+    : "https://api.pupeelms.com:4000/"; // Production (use the same base URL)
 
 axios.defaults.withCredentials = true; // Ensure credentials (cookies, auth headers) are sent with each request if necessary
 
 export const imageBaseURL =
   window.location.hostname === "localhost"
-    ? "https://52.221.193.216:4000" // Development (local)
-    : "https://52.221.193.216:4000"; // Production (same image base URL)
+    ? "https://api.pupeelms.com:4000" // Development (local)
+    : "https://api.pupeelms.com:4000"; // Production (same image base URL)
 
 export default axios;
